bafb70ff9795aaf17fcaa82b46c8efe3873ffc4e
MaksymukNatalia <maksymuknataliaa@gmail.com>
2024-02-25 16:05:43 +0100

    Add read code in file_datasource.py

--- lab_1/src/file_datasource.py
+++ lab_1/src/file_datasource.py
@@ -1,10 +1,11 @@
+import sys
 from csv import reader
 from datetime import datetime
 from domain.accelerometer import Accelerometer
 from domain.gps import Gps
 from domain.aggregated_data import AggregatedData
 import config
-
+from typing import List
 
 class FileDatasource:
     def __init__(
@@ -12,19 +13,43 @@ class FileDatasource:
         accelerometer_filename: str,
         gps_filename: str,
     ) -> None:
+        self.accelerometer_filename = accelerometer_filename
+        self.gps_filename = gps_filename
+        self.accelerometer_data = None
+        self.gps_data = None
+        self.count = 0
         pass
-
-    def read(self) -> AggregatedData:
-        """Метод повертає дані отримані з датчиків"""
-        return AggregatedData(
-            Accelerometer(1, 2, 3),
-            Gps(4, 5),
-            datetime.now(),
-            config.USER_ID,
-        )
-
     def startReading(self, *args, **kwargs):
         """Метод повинен викликатись перед початком читання даних"""
+        file = open(self.accelerometer_filename)
+        self.accelerometer_data = reader(file)
+        print(next(self.accelerometer_data))
+
+        file2 = open(self.gps_filename)
+        self.gps_data = reader(file2)
+        print(next(self.gps_data))
+
+    def read(self) -> List[AggregatedData]:
+        """Метод повертає дані отримані з датчиків"""
+        data_return = []
+
+        ac = next(self.accelerometer_data)
+        gp = next(self.gps_data)
+        da = AggregatedData(
+            Accelerometer(int(ac[0]), int(ac[1]), int(ac[2])),
+            Gps(float(gp[0]), float(gp[1])),
+            datetime.now(),
+            config.USER_ID)
+        data_return.append(da)
+        self.count+=1
+        if self.count == 125:
+            print("********")
+            sys.exit()
+        return da
+
+
+
 
     def stopReading(self, *args, **kwargs):
         """Метод повинен викликатись для закінчення читання даних"""
+      #  .close()
