2c92cf45a60902f48ac20228a4b2aff37f8a1ffd
MaksymukNatalia <maksymuknataliaa@gmail.com>
2024-02-25 17:35:01 +0100

    Add parking sensor

--- lab_1/src/file_datasource.py
+++ lab_1/src/file_datasource.py
@@ -7,27 +7,38 @@ from domain.aggregated_data import AggregatedData
 import config
 from typing import List
 
+from domain.parking import Parking
+
+
 class FileDatasource:
     def __init__(
         self,
         accelerometer_filename: str,
         gps_filename: str,
+        parking_filename: str
     ) -> None:
         self.accelerometer_filename = accelerometer_filename
         self.gps_filename = gps_filename
         self.accelerometer_data = None
         self.gps_data = None
         self.count = 0
+        self.parking_filename=parking_filename
+        self.parking_data = None
         pass
+
     def startReading(self, *args, **kwargs):
         """Метод повинен викликатись перед початком читання даних"""
         file = open(self.accelerometer_filename)
         self.accelerometer_data = reader(file)
-        print(next(self.accelerometer_data))
+        next(self.accelerometer_data)
 
         file2 = open(self.gps_filename)
         self.gps_data = reader(file2)
-        print(next(self.gps_data))
+        next(self.gps_data)
+
+        file3 = open(self.parking_filename)
+        self.parking_data = reader(file3)
+        next(self.parking_data)
 
     def read(self) -> List[AggregatedData]:
         """Метод повертає дані отримані з датчиків"""
@@ -35,19 +46,19 @@ class FileDatasource:
 
         ac = next(self.accelerometer_data)
         gp = next(self.gps_data)
+        pa = next(self.parking_data)
         da = AggregatedData(
             Accelerometer(int(ac[0]), int(ac[1]), int(ac[2])),
             Gps(float(gp[0]), float(gp[1])),
+            Parking(int(pa[0]), Gps(float(pa[1]), float(pa[2]))),
             datetime.now(),
             config.USER_ID)
         data_return.append(da)
         self.count+=1
-        if self.count == 125:
-            print("********")
+        if self.count == 40:
+            print("END")
             sys.exit()
         return da
-
-
 
 
     def stopReading(self, *args, **kwargs):
