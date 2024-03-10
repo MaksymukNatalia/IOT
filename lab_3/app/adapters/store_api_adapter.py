5a65fca196c0556cfa564c87dc2af391cba30731
MaksymukNatalia <maksymuknataliaa@gmail.com>
2024-03-10 18:02:36 +0100

    Fix syntax err

--- lab_3/app/adapters/store_api_adapter.py
+++ lab_3/app/adapters/store_api_adapter.py
@@ -4,10 +4,11 @@ from typing import List
 import pydantic_core
 import requests
 from app.entities.processed_agent_data import ProcessedAgentData
-from app.interfaces.store_gateway import StoreGateway
+from app.interfaces.store_api_gateway import StoreGateway
 
 class StoreApiAdapter(StoreGateway):
     def __init__(self, api_base_url):
         self.api_base_url = api_base_url
     def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]):
+        pass
         # Make a POST request to the Store API endpoint with the processed data
--- lab_3/config.py
+++ lab_3/config.py
@@ -8,8 +8,7 @@ def try_parse_int(value: str):
 
 # Configuration for the Store API
 STORE_API_HOST = os.environ.get("STORE_API_HOST") or "localhost"
-STORE_API_PORT = try_parse_int(os.environ.get("STORE_API_PORT")) or
-8000
+STORE_API_PORT = try_parse_int(os.environ.get("STORE_API_PORT")) or 8000
 STORE_API_BASE_URL = f"http://{STORE_API_HOST}:{STORE_API_PORT}"
 # Configure for Redis
 REDIS_HOST = os.environ.get("REDIS_HOST") or "localhost"
@@ -18,7 +17,5 @@ REDIS_PORT = try_parse_int(os.environ.get("REDIS_PORT")) or 6379
 BATCH_SIZE = try_parse_int(os.environ.get("BATCH_SIZE")) or 20
 # MQTT
 MQTT_BROKER_HOST = os.environ.get("MQTT_BROKER_HOST") or "localhost"
-MQTT_BROKER_PORT =
-try_parse_int(os.environ.get("MQTT_BROKER_PORT")) or 1883
-MQTT_TOPIC = os.environ.get("MQTT_TOPIC") or
-"processed_agent_data_topic"
+MQTT_BROKER_PORT =try_parse_int(os.environ.get("MQTT_BROKER_PORT")) or 1883
+MQTT_TOPIC = os.environ.get("MQTT_TOPIC") or "processed_agent_data_topic"
