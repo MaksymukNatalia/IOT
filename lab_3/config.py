12ecb709159beefb3d8caba208e7fa688207414d
MaksymukNatalia <maksymuknataliaa@gmail.com>
2024-03-10 18:37:26 +0100

    Fix all err

--- lab_3/config.py
+++ lab_3/config.py
@@ -16,6 +16,7 @@ REDIS_PORT = try_parse_int(os.environ.get("REDIS_PORT")) or 6379
 # Configure for hub logic
 BATCH_SIZE = try_parse_int(os.environ.get("BATCH_SIZE")) or 20
 # MQTT
+#MQTT_BROKER_HOST = os.environ.get("MQTT_BROKER_HOST") or "localhost"
 MQTT_BROKER_HOST = os.environ.get("MQTT_BROKER_HOST") or "localhost"
 MQTT_BROKER_PORT =try_parse_int(os.environ.get("MQTT_BROKER_PORT")) or 1883
 MQTT_TOPIC = os.environ.get("MQTT_TOPIC") or "processed_agent_data_topic"
